3e04563d77d1c9a949eedfb43cab0457bd3aaaa7
dramos <dario.fenix@gmail.com>
2017-06-27 20:38:57 -0300

    Transaction idiomatic RAII wrapper

---
+++ api/bitprim/Transaction.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Transaction : IDisposable
+{
+    private IntPtr nativeInstance_;
+
+    public Transaction(IntPtr nativeInstance)
+    {
+        nativeInstance_ = nativeInstance;
+    }
+
+    ~Transaction()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing){
+            //Release managed resources and call Dispose for member variables
+        }   
+        //Release unmanaged resources
+        TransactionNative.transaction_destruct(nativeInstance_);
+    }
+
+}
