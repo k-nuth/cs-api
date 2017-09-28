2b73d8d50cfcc39ac187a04c4026493210ac3bc5
Dario Ramos <dario.fenix@gmail.com>
2017-09-28 17:56:59 -0300

    DEV-72: Try simple build

--- build.sh
+++ build.sh
@@ -11,15 +11,17 @@ fi
 
 cd api
 
-dotnet restore
+# dotnet restore
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet test api.csproj -c Release -f netcoreapp1.0
+#dotnet test api.csproj -c Release -f netcoreapp1.0
 
 # Instead, run directly with mono for the full .net version 
-dotnet build api.csproj -c Release -f net451
+#dotnet build api.csproj -c Release -f net451
+
+dotnet build
 
 # TODO Run tests
 #mono \  
