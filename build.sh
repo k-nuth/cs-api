32783dc5e3617c34fba666e649f1c8f94940a908
Dario Ramos <dario.fenix@gmail.com>
2017-09-28 17:46:26 -0300

    DEV-72: Try building from the api dir

--- build.sh
+++ build.sh
@@ -9,15 +9,17 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
+cd api
+
 dotnet restore
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet test ./api/api.csproj -c Release -f netcoreapp1.0
+dotnet test api.csproj -c Release -f netcoreapp1.0
 
 # Instead, run directly with mono for the full .net version 
-dotnet build ./api/api.csproj -c Release -f net451
+dotnet build api.csproj -c Release -f net451
 
 # TODO Run tests
 #mono \  
