365d490de945a99c2058a0cc2dd021925afbb216
dario_ramos <Dario.Ramos@tellago.com>
2017-11-13 18:55:03 -0300

    T50: MerkleBlock docs

--- bitprim/idiomatic/MerkleBlock.cs
+++ bitprim/idiomatic/MerkleBlock.cs
@@ -5,84 +5,112 @@ using Bitprim.Native;
 namespace Bitprim
 {
 
-public class MerkleBlock : IDisposable
-{
-    private IntPtr nativeInstance_;
-
-    ~MerkleBlock()
+    /// <summary>
+    /// Merkle tree representation of a blockchain block.
+    /// </summary>
+    public class MerkleBlock : IDisposable
     {
-        Dispose(false);
-    }
+        private IntPtr nativeInstance_;
 
-    public bool IsValid
-    {
-        get
+        ~MerkleBlock()
         {
-            return MerkleBlockNative.chain_merkle_block_is_valid(nativeInstance_) != 0;
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Returns true if and only if it the block contains txs hashes, and the header is valid.
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return MerkleBlockNative.chain_merkle_block_is_valid(nativeInstance_) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Get the Nth transaction hash from the block.
+        /// </summary>
+        /// <param name="n">Zerp-based index.</param>
+        /// <returns> Transaction hash in 32 byte array format. </returns>
+        public byte[] GetNthHash(int n)
+        {
+            var managedHash = new hash_t();
+            MerkleBlockNative.chain_merkle_block_hash_nth_out(nativeInstance_, (UIntPtr)n, ref managedHash);
+            return managedHash.hash;
+        }
+
+        /// <summary>
+        /// The block's header.
+        /// </summary>
+        public Header Header
+        {
+            get
+            {
+                return new Header(MerkleBlockNative.chain_merkle_block_header(nativeInstance_));
+            }
+        }
+
+        /// <summary>
+        /// Transaction hashes list element count.
+        /// </summary>
+        public UInt64 HashCount
+        {
+            get
+            {
+                return (UInt64)MerkleBlockNative.chain_merkle_block_hash_count(nativeInstance_);
+            }
+        }
+
+        /// <summary>
+        /// Amount of transactions inside the block.
+        /// </summary>
+        public UInt64 TotalTransactionCount
+        {
+            get
+            {
+                return (UInt64)MerkleBlockNative.chain_merkle_block_total_transaction_count(nativeInstance_);
+            }
+        }
+
+        /// <summary>
+        /// Block size in bytes (as a Merkle block, not as a full block).
+        /// </summary>
+        /// <param name="version"> Protocol version to consider when calculating size. </param>
+        /// <returns> Size in bytes. </returns>
+        public UInt64 GetSerializedSize(UInt32 version)
+        {
+            return (UInt64)MerkleBlockNative.chain_merkle_block_serialized_size(nativeInstance_, version);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Delete all the data inside the block.
+        /// </summary>
+        public void Reset()
+        {
+            MerkleBlockNative.chain_merkle_block_reset(nativeInstance_);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Release managed resources and call Dispose for member variables
+            }
+            //Release unmanaged resources
+            MerkleBlockNative.chain_merkle_block_destruct(nativeInstance_);
+        }
+
+        internal MerkleBlock(IntPtr nativeInstance)
+        {
+            nativeInstance_ = nativeInstance;
         }
     }
-
-    public byte[] GetNthHash(int n)
-    {
-        var managedHash = new hash_t();
-        MerkleBlockNative.chain_merkle_block_hash_nth_out(nativeInstance_, (UIntPtr) n, ref managedHash);
-        return managedHash.hash;
-    }
-
-    public Header Header
-    {
-        get
-        {
-            return new Header(MerkleBlockNative.chain_merkle_block_header(nativeInstance_));
-        }
-    }
-
-    public UInt64 HashCount
-    {
-        get
-        {
-            return (UInt64) MerkleBlockNative.chain_merkle_block_hash_count(nativeInstance_);
-        }
-    }
-
-    public UInt64 TotalTransactionCount
-    {
-        get
-        {
-            return (UInt64) MerkleBlockNative.chain_merkle_block_total_transaction_count(nativeInstance_);
-        }
-    }
-
-    public UInt64 GetSerializedSize(UInt32 version)
-    {
-        return (UInt64) MerkleBlockNative.chain_merkle_block_serialized_size(nativeInstance_, version);
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    public void Reset()
-    {
-        MerkleBlockNative.chain_merkle_block_reset(nativeInstance_);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            //Release managed resources and call Dispose for member variables
-        }   
-        //Release unmanaged resources
-        MerkleBlockNative.chain_merkle_block_destruct(nativeInstance_);
-    }
-
-    internal MerkleBlock(IntPtr nativeInstance)
-    {
-        nativeInstance_ = nativeInstance;
-    }
-}
 
 }
