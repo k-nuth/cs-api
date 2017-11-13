3cad3c783f293e837447c3808bad72fc8021f991
dario_ramos <Dario.Ramos@tellago.com>
2017-11-13 19:22:12 -0300

    T50: Script docs

--- bitprim/idiomatic/Script.cs
+++ bitprim/idiomatic/Script.cs
@@ -3,83 +3,111 @@ using Bitprim.Native;
 
 namespace Bitprim
 {
-
-public class Script : IDisposable
-{
-    private IntPtr nativeInstance_;
-
-   ~Script()
+    /// <summary>
+    /// Represents a transaction script.
+    /// </summary>
+    public class Script : IDisposable
     {
-        Dispose(false);
-    }
+        private IntPtr nativeInstance_;
 
-    internal Script(IntPtr nativeInstance)
-    {
-        nativeInstance_ = nativeInstance;
-    }
-
-    public bool IsValid
-    {
-        get
+        ~Script()
         {
-            return ScriptNative.chain_script_is_valid(nativeInstance_) != 0;
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// All script bytes are valid under some circumstance (e.g. coinbase).
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return ScriptNative.chain_script_is_valid(nativeInstance_) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Script validity is independent of individual operation validity. 
+        /// Operations are considered invalid if there is a trailing invalid/default op
+        /// or if a push op has a size mismatch.
+        /// </summary>
+        public bool OperationsAreValid
+        {
+            get
+            {
+                return ScriptNative.chain_script_is_valid_operations(nativeInstance_) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Translate operations in the script to a string.
+        /// </summary>
+        /// <param name="activeForks"> Tells which rule is active. </param>
+        /// <returns> Human readable script. </returns>
+        public string ToString(UInt32 activeForks)
+        {
+            return ScriptNative.chain_script_to_string(nativeInstance_, activeForks);
+        }
+
+        /// <summary>
+        /// Size in bytes.
+        /// </summary>
+        public UIntPtr SatoshiContentSize
+        {
+            get
+            {
+                return ScriptNative.chain_script_satoshi_content_size(nativeInstance_);
+            }
+        }
+
+        /// <summary>
+        /// Count the sigops in the embedded script using BIP16 rules.
+        /// </summary>
+        /// <param name="prevOutScript"> Reference to previous output script. </param>
+        /// <returns> Embedded sigops count. </returns>
+        public UIntPtr GetEmbeddedSigOps(Script prevOutScript)
+        {
+            return ScriptNative.chain_script_embedded_sigops(nativeInstance_, prevOutScript.nativeInstance_);
+        }
+
+        /// <summary>
+        /// Amount of signature operations in the script.
+        /// </summary>
+        /// <param name="embedded"> Iif true, consider this an embedded script. </param>
+        /// <returns> Sigops count. </returns>
+        public UIntPtr GetSigOps(bool embedded)
+        {
+            return ScriptNative.chain_script_sigops(nativeInstance_, embedded ? 1 : 0);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Release managed resources and call Dispose for member variables
+            }
+            //Release unmanaged resources
+            ScriptNative.chain_script_destruct(nativeInstance_);
+        }
+
+        internal Script(IntPtr nativeInstance)
+        {
+            nativeInstance_ = nativeInstance;
+        }
+
+        internal IntPtr NativeInstance
+        {
+            get
+            {
+                return nativeInstance_;
+            }
         }
     }
-
-    public bool OperationsAreValid
-    {
-        get
-        {
-            return ScriptNative.chain_script_is_valid_operations(nativeInstance_) != 0;
-        }
-    }
-
-    public string ToString(UInt32 activeForks)
-    {
-        return ScriptNative.chain_script_to_string(nativeInstance_, activeForks);
-    }
-
-    public UIntPtr SatoshiContentSize
-    {
-        get
-        {
-            return ScriptNative.chain_script_satoshi_content_size(nativeInstance_);
-        }
-    }
-
-    public UIntPtr GetEmbeddedSigOps(Script prevOutScript)
-    {
-        return ScriptNative.chain_script_embedded_sigops(nativeInstance_, prevOutScript.nativeInstance_);
-    }
-
-    public UIntPtr GetSigOps(bool embedded)
-    {
-        return ScriptNative.chain_script_sigops(nativeInstance_, embedded? 1:0);
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            //Release managed resources and call Dispose for member variables
-        }   
-        //Release unmanaged resources
-        ScriptNative.chain_script_destruct(nativeInstance_);
-    }
-
-    internal IntPtr NativeInstance
-    {
-        get
-        {
-            return nativeInstance_;
-        }
-    }
-}
 
 }
