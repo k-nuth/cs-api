1e3ec8caa7885ad0230517f5358204e023734819
dario_ramos <Dario.Ramos@tellago.com>
2017-11-13 19:01:02 -0300

    T50: OutputPoint docs, plus cleanup

--- bitprim/idiomatic/OutputList.cs
+++ bitprim/idiomatic/OutputList.cs
@@ -5,98 +5,98 @@ using System.Collections;
 namespace Bitprim
 {
 
-public class OutputList : IDisposable
-{
-
-    private IntPtr nativeInstance_;
-
-    public OutputList()
+    public class OutputList : IDisposable
     {
-        nativeInstance_ = OutputListNative.chain_output_list_construct_default();
-    }
 
-    internal OutputList(IntPtr nativeInstance)
-    {
-        nativeInstance_ = nativeInstance;
-    }
+        private IntPtr nativeInstance_;
 
-    ~OutputList()
-    {
-        Dispose(false);
-    }
-
-    public IEnumerator GetEnumerator()
-    {
-        return new OutputListEnumerator(nativeInstance_);
-    }
-
-    public uint Count
-    {
-        get
+        public OutputList()
         {
-            return (uint) OutputListNative.chain_output_list_count(nativeInstance_);
+            nativeInstance_ = OutputListNative.chain_output_list_construct_default();
+        }
+
+        internal OutputList(IntPtr nativeInstance)
+        {
+            nativeInstance_ = nativeInstance;
+        }
+
+        ~OutputList()
+        {
+            Dispose(false);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new OutputListEnumerator(nativeInstance_);
+        }
+
+        public uint Count
+        {
+            get
+            {
+                return (uint)OutputListNative.chain_output_list_count(nativeInstance_);
+            }
+        }
+
+        public void Add(Output output)
+        {
+            OutputListNative.chain_output_list_push_back(nativeInstance_, output.NativeInstance);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Release managed resources and call Dispose for member variables
+            }
+            //Release unmanaged resources
+            OutputListNative.chain_output_list_destruct(nativeInstance_);
+        }
+
+        internal IntPtr NativeInstance
+        {
+            get
+            {
+                return nativeInstance_;
+            }
         }
     }
 
-    public void Add(Output output)
+    public class OutputListEnumerator : IEnumerator
     {
-        OutputListNative.chain_output_list_push_back(nativeInstance_, output.NativeInstance);
-    }
+        private uint counter_;
+        private IntPtr nativeCollection_;
 
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
+        public OutputListEnumerator(IntPtr nativeCollection)
         {
-            //Release managed resources and call Dispose for member variables
-        }   
-        //Release unmanaged resources
-        OutputListNative.chain_output_list_destruct(nativeInstance_);
-    }
+            nativeCollection_ = nativeCollection;
+            counter_ = 0;
+        }
 
-    internal IntPtr NativeInstance
-    {
-        get
+        public bool MoveNext()
         {
-            return nativeInstance_;
+            counter_++;
+            return counter_ != (uint)InputListNative.chain_input_list_count(nativeCollection_);
+        }
+
+        public object Current
+        {
+            get
+            {
+                return new Input(InputListNative.chain_input_list_nth(nativeCollection_, (UIntPtr)counter_));
+            }
+        }
+
+        public void Reset()
+        {
+            counter_ = 0;
         }
     }
-}
-
-public class OutputListEnumerator : IEnumerator
-{
-    private uint counter_;
-    private IntPtr nativeCollection_;
-
-    public OutputListEnumerator(IntPtr nativeCollection)
-    {
-        nativeCollection_ = nativeCollection;
-        counter_ = 0;
-    }
-
-    public bool MoveNext()
-    {
-        counter_++;
-        return counter_ != (uint) InputListNative.chain_input_list_count(nativeCollection_);
-    }
-
-    public object Current
-    {
-        get
-        {
-            return new Input(InputListNative.chain_input_list_nth(nativeCollection_, (UIntPtr) counter_));
-        }
-    }
-
-    public void Reset()
-    {
-        counter_ = 0;
-    }
-}
 
 }
