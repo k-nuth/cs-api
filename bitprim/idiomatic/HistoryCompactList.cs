194e06ca3a6bd4ed8b659e254314e84c23dbab28
dario_ramos <Dario.Ramos@tellago.com>
2017-11-13 18:26:46 -0300

    T50: Input docs, small cleanup

--- bitprim/idiomatic/HistoryCompactList.cs
+++ bitprim/idiomatic/HistoryCompactList.cs
@@ -5,89 +5,89 @@ using System.Collections;
 namespace Bitprim
 {
 
-public class HistoryCompactList : IDisposable
-{
-
-    private IntPtr nativeInstance_;
-
-    ~HistoryCompactList()
+    public class HistoryCompactList : IDisposable
     {
-        Dispose(false);
-    }
 
-    public IEnumerator GetEnumerator()
-    {
-        return new HistoryCompactListEnumerator(nativeInstance_);
-    }
+        private IntPtr nativeInstance_;
 
-    public uint Count
-    {
-        get
+        ~HistoryCompactList()
         {
-            return (uint) HistoryCompactListNative.chain_history_compact_list_count(nativeInstance_);
+            Dispose(false);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new HistoryCompactListEnumerator(nativeInstance_);
+        }
+
+        public uint Count
+        {
+            get
+            {
+                return (uint)HistoryCompactListNative.chain_history_compact_list_count(nativeInstance_);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Release managed resources and call Dispose for member variables
+            }
+            //Release unmanaged resources
+            HistoryCompactListNative.chain_history_compact_list_destruct(nativeInstance_);
+        }
+
+        internal HistoryCompactList(IntPtr nativeInstance)
+        {
+            nativeInstance_ = nativeInstance;
+        }
+
+        internal IntPtr NativeInstance
+        {
+            get
+            {
+                return nativeInstance_;
+            }
         }
     }
 
-    public void Dispose()
+    public class HistoryCompactListEnumerator : IEnumerator
     {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
+        private UInt64 counter_;
+        private IntPtr nativeCollection_;
 
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
+        public bool MoveNext()
         {
-            //Release managed resources and call Dispose for member variables
-        }   
-        //Release unmanaged resources
-        HistoryCompactListNative.chain_history_compact_list_destruct(nativeInstance_);
-    }
-
-    internal HistoryCompactList(IntPtr nativeInstance)
-    {
-        nativeInstance_ = nativeInstance;
-    }
-
-    internal IntPtr NativeInstance
-    {
-        get
-        {
-            return nativeInstance_;
+            counter_++;
+            return counter_ != (uint)HistoryCompactListNative.chain_history_compact_list_count(nativeCollection_);
         }
-    }
-}
 
-public class HistoryCompactListEnumerator : IEnumerator
-{
-    private UInt64 counter_;
-    private IntPtr nativeCollection_;
-
-    public HistoryCompactListEnumerator(IntPtr nativeCollection)
-    {
-        nativeCollection_ = nativeCollection;
-        counter_ = 0;
-    }
-
-    public bool MoveNext()
-    {
-        counter_++;
-        return counter_ != (uint) HistoryCompactListNative.chain_history_compact_list_count(nativeCollection_);
-    }
-
-    public object Current
-    {
-        get
+        public object Current
         {
-            return HistoryCompactListNative.chain_history_compact_list_nth(nativeCollection_, counter_);
+            get
+            {
+                return HistoryCompactListNative.chain_history_compact_list_nth(nativeCollection_, counter_);
+            }
         }
-    }
 
-    public void Reset()
-    {
-        counter_ = 0;
-    }
+        public void Reset()
+        {
+            counter_ = 0;
+        }
 
-}
+        internal HistoryCompactListEnumerator(IntPtr nativeCollection)
+        {
+            nativeCollection_ = nativeCollection;
+            counter_ = 0;
+        }
+
+    }
 
 }
--- bitprim/idiomatic/Input.cs
+++ bitprim/idiomatic/Input.cs
@@ -4,103 +4,140 @@ using Bitprim.Native;
 namespace Bitprim
 {
 
-public class Input : IDisposable
-{
-    private IntPtr nativeInstance_;
-
-    public Input()
+    /// <summary>
+    /// Represents a Transaction input.
+    /// </summary>
+    public class Input : IDisposable
     {
-        nativeInstance_ = InputNative.chain_input_construct_default();
-    }
+        private IntPtr nativeInstance_;
 
-    public Input(Output previousOutput, Script script, UInt32 sequence)
-    {
-        nativeInstance_ = InputNative.chain_input_construct(previousOutput.NativeInstance, script.NativeInstance, sequence);
-    }
-
-    internal Input(IntPtr nativeInstance)
-    {
-        nativeInstance_ = nativeInstance;
-    }
-
-    ~Input()
-    {
-        Dispose(false);
-    }
-
-    public bool IsFinal
-    {
-        get
+        /// <summary>
+        /// Create an empty input.
+        /// </summary>
+        public Input()
         {
-            return InputNative.chain_input_is_final(nativeInstance_) != 0;
+            nativeInstance_ = InputNative.chain_input_construct_default();
+        }
+
+        /// <summary>
+        /// Create an input from a previous output, a script and a sequence number.
+        /// </summary>
+        /// <param name="previousOutput"> The output to spend. </param>
+        /// <param name="script"> Input script. </param>
+        /// <param name="sequence"> Zero-based, indexes this input in the input set. </param>
+        public Input(Output previousOutput, Script script, UInt32 sequence)
+        {
+            nativeInstance_ = InputNative.chain_input_construct(previousOutput.NativeInstance, script.NativeInstance, sequence);
+        }
+
+        ~Input()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Returns 1 iif sequence is equal to max_sequence.
+        /// </summary>
+        public bool IsFinal
+        {
+            get
+            {
+                return InputNative.chain_input_is_final(nativeInstance_) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Returns false if and only if previous outputs or input script are invalid.
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return InputNative.chain_input_is_valid(nativeInstance_) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Returns the previous output, with its transaction hash and index.
+        /// </summary>
+        public Output PreviousOutput
+        {
+            get
+            {
+                return new Output(InputNative.chain_input_previous_output(nativeInstance_));
+            }
+        }
+
+        /// <summary>
+        /// The input's script.
+        /// </summary>
+        public Script Script
+        {
+            get
+            {
+                return new Script(InputNative.chain_input_script(nativeInstance_));
+            }
+        }
+
+        /// <summary>
+        /// Zero-based index for the input in the transaction's input set.
+        /// </summary>
+        public UInt32 Sequence
+        {
+            get
+            {
+                return InputNative.chain_input_sequence(nativeInstance_);
+            }
+        }
+
+        /// <summary>
+        /// Input size in bytes.
+        /// </summary>
+        /// <param name="wire"> Iif true, consider 4 extra bytes from wire field. </param>
+        /// <returns> Size in bytes. </returns>
+        public UIntPtr GetSerializedSize(bool wire)
+        {
+            return InputNative.chain_input_serialized_size(nativeInstance_, wire ? 1 : 0);
+        }
+
+        /// <summary>
+        /// Total amount of sigops (signature operations) in the input script.
+        /// </summary>
+        /// <param name="bip16Active"> Iif true, count BIP 16 active sig ops</param>
+        /// <returns> Sigops count. </returns>
+        public UIntPtr GetSignatureOperationsCount(bool bip16Active)
+        {
+            return InputNative.chain_input_signature_operations(nativeInstance_, bip16Active ? 1 : 0);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Release managed resources and call Dispose for member variables
+            }
+            //Release unmanaged resources
+            InputNative.chain_input_destruct(nativeInstance_);
+        }
+
+        internal Input(IntPtr nativeInstance)
+        {
+            nativeInstance_ = nativeInstance;
+        }
+
+        internal IntPtr NativeInstance
+        {
+            get
+            {
+                return nativeInstance_;
+            }
         }
     }
-
-    public bool IsValid
-    {
-        get
-        {
-            return InputNative.chain_input_is_valid(nativeInstance_) != 0;
-        }
-    }
-
-    public Output PreviousOutput
-    {
-        get
-        {
-            return new Output(InputNative.chain_input_previous_output(nativeInstance_));
-        }
-    }
-
-    public Script Script
-    {
-        get
-        {
-            return new Script(InputNative.chain_input_script(nativeInstance_));
-        }
-    }
-
-    public UInt32 Sequence
-    {
-        get
-        {
-            return InputNative.chain_input_sequence(nativeInstance_);
-        }
-    }
-
-    public UIntPtr GetSerializedSize(bool wire)
-    {
-        return InputNative.chain_input_serialized_size(nativeInstance_, wire? 1:0);
-    }
-
-    public UIntPtr GetSignatureOperationsCount(bool bip16Active)
-    {
-        return InputNative.chain_input_signature_operations(nativeInstance_, bip16Active? 1:0);
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            //Release managed resources and call Dispose for member variables
-        }   
-        //Release unmanaged resources
-        InputNative.chain_input_destruct(nativeInstance_);
-    }
-
-    internal IntPtr NativeInstance
-    {
-        get
-        {
-            return nativeInstance_;
-        }
-    }
-}
 
 }
