5536a68b13aa9f49d036b4f4b0da6f595b4c6d66
dario-ramos <dario.fenix@gmail.com>
2017-11-27 13:13:40 -0300

    T60: Use a timestamp to increment version

--- increment_version.ps1
+++ increment_version.ps1
@@ -1,1 +1,4 @@
-Write-Host "Incrementing version"
+Write-Host "Incrementing version..."
+(Get-Content -path "bitprim\bitprim.csproj") -replace '<Version>([0-9\.]*)</Version>', '<Version>$1-pre%%TIMESTAMP_PLACEHOLDER%%</Version>' |  Set-Content "bitprim\bitprim.csproj"
+$timestamp = Get-Date -format yyyyMMddHHmmss
+(Get-Content -path "bitprim\bitprim.csproj") -replace '%%TIMESTAMP_PLACEHOLDER%%', "$timestamp" |  Set-Content "bitprim\bitprim.csproj"
