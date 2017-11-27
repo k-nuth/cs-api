ed5ecc9bad624af503561a93fca7b043bbd3d342
dario-ramos <dario.fenix@gmail.com>
2017-11-27 15:26:34 -0300

    T60: Don't use timestamp; just increment patch version

--- increment_version.ps1
+++ increment_version.ps1
@@ -1,4 +1,10 @@
 Write-Host "Incrementing version..."
-(Get-Content -path "bitprim\bitprim.csproj") -replace '<Version>([0-9\.]*)</Version>', '<Version>$1-pre%%TIMESTAMP_PLACEHOLDER%%</Version>' |  Set-Content "bitprim\bitprim.csproj"
-$timestamp = Get-Date -format yyyyMMddHHmmss
-(Get-Content -path "bitprim\bitprim.csproj") -replace '%%TIMESTAMP_PLACEHOLDER%%', "$timestamp" |  Set-Content "bitprim\bitprim.csproj"
+# Get the current patch version
+[regex]$rx='<Version>([0-9]+)\.([0-9]+)\.([0-9]+)</Version>'
+$patch = $rx.Match([IO.File]::ReadAllText("bitprim\bitprim.csproj")).captures.groups[3].value
+# Add 1 to it
+$nextVersion = [int]$patch + 1;
+# Replace the new value in the project metadata
+(Get-Content -path "bitprim\bitprim.csproj") -replace '<Version>([0-9]+)\.([0-9]+)\.([0-9]+)</Version>', '<Version>$1.$2.%%PATCH_PLACEHOLDER%%</Version>' |  Set-Content "bitprim\bitprim.csproj"
+(Get-Content -path "bitprim\bitprim.csproj") -replace '%%PATCH_PLACEHOLDER%%', "$nextVersion" |  Set-Content "bitprim\bitprim.csproj"
+Write-Host "Patch version updated to $nextVersion"
