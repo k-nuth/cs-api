47db1879bff86b8ae996fbe58403ad0ad9e98c44
dario-ramos <dario.fenix@gmail.com>
2017-11-29 15:27:15 -0300

    T68: Travis build fix

--- build.sh
+++ build.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-#Exit if any command fails
-set -e
-
-#Build webtest project (this build bitprim-cs class library as a dependency)
-cd webtest
-dotnet restore webtest.csproj
-dotnet build webtest.csproj -c Release
-
 #Run tests
-cd ..
-#Copy native dependencies
-cp bitprim/* ./bitprim-cs.tests/bin/Release/netcoreapp1.1 
-cd bitprim-cs.tests
+cd bitprim.tests
 dotnet restore
 dotnet test
