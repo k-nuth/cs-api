dfb501d5f9fa3240faade0795a824f4ede6db6ae
dario-ramos <dario.fenix@gmail.com>
2017-12-04 17:17:57 -0300

    T68: Release build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 #Run tests
 cd bitprim.tests
-dotnet build /property:Platform=x64 -f netcoreapp2.0 -v normal
-dotnet test /property:Platform=x64 -f netcoreapp2.0 -v normal
+dotnet build /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
+dotnet test /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
