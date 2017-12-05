a99659747a82199919702ca31546c04e0cf0d9cb
dario-ramos <dario.fenix@gmail.com>
2017-12-05 11:08:15 -0300

    T68: Copy assembly from NuGet folder

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 #Run tests
 cd bitprim.tests
 dotnet build /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
-dotnet test /property:Platform=x64 -c Releasegit  -f netcoreapp2.0 -v normal
+dotnet test /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
