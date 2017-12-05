4d79489cf1c1d483aa5a9367d2a176c37da5054f
Dario Ramos <dario.fenix@gmail.com>
2017-12-05 11:29:41 -0300

    T60, T68 (#17)
    * T60: Build with new project structure
    
    * T60: Fix folder name
    
    * T60: Use a package reference in order to run the tests
    
    * T60
    
    * T60: Pack Common.Targets
    
    * T60: Fixed target names
    
    * T60: Install Conan on Appveyor
    
    * T60: Lapsus
    
    * T60: Add python scripts to path so that Conan can run
    
    * T60: Test
    
    * T60: x64 build (due to x64 native binary)
    
    * T60: x64 build
    
    * T60: x64 tests
    
    * T60: Run test for a single framework
    
    * T60: Delete blockchain dir
    
    * T60: No confirmation
    
    * T60: Current dir
    
    * T60: Test
    
    * T60: Verify error code
    
    * T60: Run single test to isolate issue
    
    * T60: Try running tests sequentially to detect issue
    
    * T60: 2nd test isolated
    
    * T60: Grant full permissions to project dir to avoid errors
    
    * T60: Grant full permissions to project dir to avoid errors
    
    * T60: Try latest Conan
    
    * T60: Try running test with newer fx
    
    * T60: Re-enable whole suite to check if testing against net core 2.0 fixes xUnit issue
    
    * T60: Fix
    
    * T60: Fix
    
    * T60: Fix
    
    * T60: Use environment variable instead of fixed path
    
    * T66: Versions file
    
    * T60: Publish to NuGet from CI
    
    * T60: nupkg was not being detected
    
    * T60: .yml syntax fix
    
    * T60: Try omitting artifact
    
    * T60: Try pushing manually to nuget.org
    
    * T60: Destination URL must be defined
    
    * T60: Fixed nupkg path
    
    * T60: Test if new version gets uploaded
    
    * T60: Test if new version gets uploaded
    
    * T60: Increment version mechanism test
    
    * T60: Increment version mechanism test
    
    * T60: Use a timestamp to increment version
    
    * T60: Delete other packages before uploading
    
    * T60: Upgrade to 0.2.7 so that pre releases follow
    
    * T60: Don't use timestamp; just increment patch version
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet from a single job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: Publish to NuGet in only one job
    
    * T60: All ps code in ps block
    
    * T60: Specify path
    
    * T60: Try retrieving latest version from nuget.org
    
    * T60: Test
    
    * T60: Try retrieving latest version from nuget.org
    
    * T60: Test
    
    * T60: Check sources
    
    * T60: Test
    
    * T60: Try a newer source URL
    
    * T60: Get latest package version from nuget.org
    
    * T60: Get latest package version from nuget.org
    
    * T68: Travis build fix
    
    * T68: Try not installing .NET Core
    
    * T68: Dotnet install is needed
    
    * T68: We were using .NET Core 1.0 install script
    
    * T68: New install steps for .NET Core 2.x
    
    * T68: Build for x64 and .NET Core 2.0
    
    * T68: Import .so native binary
    
    * T68: Reference latest nuget package
    
    * T68: Try using gcc 4.9
    
    * T68: Try setting gcc version
    
    * T68: Try setting gcc version
    
    * T68: Try setting gcc version
    
    * T68: Try setting gcc version
    
    * T68: Copy .so in Linux, .dll in Windows
    
    * T68: Get the latest NuGet package (MSBuild was updated)
    
    * T68: Git ignore
    
    * T68: Removed extraneous copy commands
    
    * T68: Print OS
    
    * T68: Lapsus
    
    * T68: Update version
    
    * T68: Fix reference
    
    * T68: Raise verbosity for debug purposes
    
    * T68: Try gcc 4.9
    
    * T68: Release build
    
    * T68: Release build
    
    * T68: Copy assembly from NuGet folder

--- build.sh
+++ build.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-#Exit if any command fails
-set -e
-
-#Build webtest project (this build bitprim-cs class library as a dependency)
-cd webtest
-dotnet restore webtest.csproj
-dotnet build webtest.csproj -c Release
-
 #Run tests
-cd ..
-#Copy native dependencies
-cp bitprim/* ./bitprim-cs.tests/bin/Release/netcoreapp1.1 
-cd bitprim-cs.tests
-dotnet restore
-dotnet test
+cd bitprim.tests
+dotnet build /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
+dotnet test /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
