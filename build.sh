bc73ba4b068315581f3dba39c691b6f8c53c5868
Dario Ramos <dario.fenix@gmail.com>
2017-12-24 17:16:01 -0300

    T74: Merge fix

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#Run tests
-cd bitprim.tests
-dotnet build /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
-dotnet test /property:Platform=x64 -c Release -f netcoreapp2.0 -v normal
