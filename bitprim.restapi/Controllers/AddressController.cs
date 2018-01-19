2ea115c89f7e8fd0a672730501ef77c81219656c
dario-ramos <dario.fenix@gmail.com>
2018-01-19 17:46:15 -0300

    T77: Draft [skip ci]

--- bitprim.restapi/Controllers/AddressController.cs
+++ bitprim.restapi/Controllers/AddressController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Bitprim;
+using System;
 
 namespace api.Controllers
 {
@@ -13,6 +14,52 @@ namespace api.Controllers
             chain_ = chain;
         }
 
-        
+        // GET: api/addr/{paymentAddress}
+        [HttpGet("/api/addr/{paymentAddress}")]
+        public ActionResult GetAddressHistory(string paymentAddress)
+        {
+            try
+            {
+                Tuple<ErrorCode, HistoryCompactList> getAddressHistoryResult = chain_.GetHistory(new PaymentAddress(paymentAddress), UInt64.MaxValue, 0);
+                Utils.CheckBitprimApiErrorCode(getAddressHistoryResult.Item1, "GetHistory(" + paymentAddress + ") failed, check error log.");
+                HistoryCompactList history = getAddressHistoryResult.Item2;
+                UInt64 adressBalance = 0;
+                UInt64 received = 0;
+                foreach(HistoryCompact compact in history)
+                {
+                    if(compact.PointKind == PointKind.Output)
+                    {
+                        received += compact.ValueOrChecksum;
+                        Tuple<ErrorCode, Point> getSpendResult = chain_.GetSpend(new OutputPoint(compact.Point.Hash, compact.Point.Index));
+                        if(getSpendResult.Item1 == ErrorCode.NotFound)
+                        {
+                            adressBalance += compact.ValueOrChecksum;
+                        }
+                    }
+                }
+                return Json
+                (
+                    new
+                    {
+                        addrStr = paymentAddress,
+                        balanceSat = Utils.SatoshisToBTC(adressBalance),
+                        totalReceived = Utils.SatoshisToBTC(received),
+                        totalReceivedSat = received,
+                        // "totalSent": 0,
+                        // "totalSentSat": 0,
+                        // "unconfirmedBalance": 0,
+                        // "unconfirmedBalanceSat": 0,
+                        // "unconfirmedTxApperances": 0,
+                        // "txApperances": 33,
+                        // "transactions":,
+                        historyCount = history.Count 
+                    }
+                );
+            }
+            catch(Exception ex)
+            {
+                return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
     }
 }
