427fa60d78902b8628480a40d5f59c413aa9efc8
dramos <dario.fenix@gmail.com>
2018-01-23 17:44:51 -0300

    T81: Test [skip ci]

--- bitprim.restapi/Controllers/AddressController.cs
+++ bitprim.restapi/Controllers/AddressController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Bitprim;
 using System;
+using System.Collections.Generic;
 
 namespace api.Controllers
 {
@@ -27,36 +28,27 @@ namespace api.Controllers
                 Tuple<ErrorCode, HistoryCompactList> getAddressHistoryResult = chain_.GetHistory(new PaymentAddress(paymentAddress), UInt64.MaxValue, 0);
                 Utils.CheckBitprimApiErrorCode(getAddressHistoryResult.Item1, "GetHistory(" + paymentAddress + ") failed, check error log.");
                 HistoryCompactList history = getAddressHistoryResult.Item2;
-                UInt64 adressBalance = 0;
-                UInt64 received = 0;
-                foreach(HistoryCompact compact in history)
-                {
-                    if(compact.PointKind == PointKind.Output)
-                    {
-                        received += compact.ValueOrChecksum;
-                        Tuple<ErrorCode, Point> getSpendResult = chain_.GetSpend(new OutputPoint(compact.Point.Hash, compact.Point.Index));
-                        if(getSpendResult.Item1 == ErrorCode.NotFound)
-                        {
-                            adressBalance += compact.ValueOrChecksum;
-                        }
-                    }
-                }
+                Tuple<UInt64, List<string>, UInt64> balance = GetBalance(history);
+                UInt64 addressBalance = balance.Item1;
+                List<string> txs = balance.Item2;
+                UInt64 received = balance.Item3;
                 return Json
                 (
                     new
                     {
                         addrStr = paymentAddress,
-                        balanceSat = Utils.SatoshisToBTC(adressBalance),
+                        balance = addressBalance,
+                        balanceSat = Utils.SatoshisToBTC(addressBalance),
                         totalReceived = Utils.SatoshisToBTC(received),
                         totalReceivedSat = received,
-                        // "totalSent": 0,
-                        // "totalSentSat": 0,
-                        // "unconfirmedBalance": 0,
-                        // "unconfirmedBalanceSat": 0,
-                        // "unconfirmedTxApperances": 0,
-                        // "txApperances": 33,
-                        // "transactions":,
-                        historyCount = history.Count,
+                        totalSent = received - addressBalance,
+                        totalSentSat = Utils.SatoshisToBTC(received - addressBalance),
+                        unconfirmedBalance = 0, //We don't handle unconfirmed txs
+                        unconfirmedBalanceSat = 0, //We don't handle unconfirmed txs
+                        unconfirmedTxApperances = 0, //We don't handle unconfirmed txs
+                        txApperances = txs.Count,
+                        transactions = txs.ToArray(),
+                        historyCount = history.Count
                         //network = NodeSettings.NetworkType.ToString(),
                         //currency = NodeSettings.CurrencyType.ToString()
                     }
@@ -66,6 +58,27 @@ namespace api.Controllers
             {
                 return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex.Message);
             }
+        }
+
+        private Tuple<UInt64, List<string>, UInt64> GetBalance(HistoryCompactList history)
+        {
+            UInt64 received = 0;
+            UInt64 addressBalance = 0;
+            var txs = new List<string>();
+            foreach(HistoryCompact compact in history)
+            {
+                if(compact.PointKind == PointKind.Output)
+                {
+                    received += compact.ValueOrChecksum;
+                    Tuple<ErrorCode, Point> getSpendResult = chain_.GetSpend(new OutputPoint(compact.Point.Hash, compact.Point.Index));
+                    txs.Add(Binary.ByteArrayToHexString(compact.Point.Hash));
+                    if(getSpendResult.Item1 == ErrorCode.NotFound)
+                    {
+                        addressBalance += compact.ValueOrChecksum;
+                    }
+                }
+            }
+            return new Tuple<UInt64, List<string>, UInt64>(addressBalance, txs, received);
         }
     }
 }
