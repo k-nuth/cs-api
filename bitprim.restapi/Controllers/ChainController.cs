8a29f9c10df8ca14d3d30f993a61f13b7213347e
dramos <dario.fenix@gmail.com>
2018-01-28 17:01:49 -0300

    T91: Network status

--- bitprim.restapi/Controllers/ChainController.cs
+++ bitprim.restapi/Controllers/ChainController.cs
@@ -11,6 +11,9 @@ namespace api.Controllers
     {
         private Chain chain_;
         private readonly NodeConfig config_;
+        private const string GET_BEST_BLOCK_HASH = "getBestBlockHash";
+        private const string GET_LAST_BLOCK_HASH = "getLastBlockHash";
+        private const string GET_DIFFICULTY = "getDifficulty";
 
         public ChainController(IOptions<NodeConfig> config, Chain chain)
         {
@@ -40,6 +43,76 @@ namespace api.Controllers
             {
                 return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex.Message);
             }
+        }
+
+        [HttpGet("api/status")]
+        public ActionResult GetStatus(string method)
+        {
+            try
+            {
+                if(method == GET_DIFFICULTY)
+                {
+                    return GetDifficulty();
+                }
+                else if(method == GET_BEST_BLOCK_HASH)
+                {
+                    return GetBestBlockHash();
+                }
+                else if(method == GET_LAST_BLOCK_HASH)
+                {
+                    return GetLastBlockHash();
+                }
+                else
+                {
+                    return GetInfo();
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        private ActionResult GetDifficulty()
+        {
+            return Json(new{});
+        }
+
+        private ActionResult GetBestBlockHash()
+        {
+            return Json(new{});
+        }
+
+        private ActionResult GetLastBlockHash()
+        {
+            return Json(new{});
+        }
+
+        private ActionResult GetInfo()
+        {
+            Tuple<ErrorCode, UInt64> getLastHeightResult = chain_.GetLastHeight();
+            Utils.CheckBitprimApiErrorCode(getLastHeightResult.Item1, "GetLastHeight() failed");
+            UInt64 currentHeight = getLastHeightResult.Item2;
+            return Json
+            (
+                new
+                {
+                    info = new 
+                    {
+                        //version = 120100, //TODO
+                        //protocolversion = 70012, //TODO
+                        blocks = currentHeight,
+                        //timeoffset = 0, //TODO
+                        //connections = 8, //TODO
+                        //proxy = "", //TODO
+                        //difficulty = 2108481.043832448, //TODO
+                        testnet = /*NodeSettings.UseTestnetRules*/true //TODO Use NodeSettings when node-cint fixed
+                        //relayfee = 0.00001, //TODO
+                        //errors = "Warning: unknown new rules activated (versionbit 28)", //TODO
+                    },
+                    network = /*NodeSettings.NetworkType.ToString()*/ "testnet" //TODO Use NodeSettings when node-cint fixed
+                }
+            );
         }
     }
 }
