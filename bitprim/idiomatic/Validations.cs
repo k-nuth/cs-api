790a23011bad44a01e30458ca1939d59238c9772
dario-ramos <dario.fenix@gmail.com>
2018-02-15 11:43:31 -0300

    RA-1: Forgot file [skip ci]

---
+++ bitprim/idiomatic/Validations.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+
+namespace Bitprim
+{
+    public static class Validations
+    {
+        private const int HASH_HEX_LENGTH = 64;
+
+        /// <summary>
+        /// Check if the hex string represents a valid base58 address
+        /// </summary>
+        /// <param name="hex">Hex string</param>
+        /// <returns>True iif hex is a valid base 58 address</returns>
+        public static bool IsValidBase58Address(string hex)
+        {
+            var address = new PaymentAddress(hex);
+            return address.IsValid;
+        }
+
+        /// <summary>
+        /// Returns true iif hex represents a valid Bitcoin hash
+        /// (i.e. 32 arbitrary bytes => 64 hex characters)
+        /// </summary>
+        /// <param name="hex">Hex string</param>
+        /// <returns>True iif hex is a valid hash string</returns>
+        public static bool IsValidHash(string hex)
+        {
+            if(string.IsNullOrWhiteSpace(hex) || hex.Length != HASH_HEX_LENGTH)
+            {
+                return false;
+            }
+            //Check if it is a hex string
+            return hex.All("0123456789abcdefABCDEF".Contains);
+        }
+    }
+}
