5a5164305730600e885d0b5e767a2064f6704a50
Mario Dal Lago <the4power@yahoo.com.ar>
2018-05-11 15:59:38 -0300

    added tests

---
+++ bitprim.tests.btc/ExecutorTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Bitprim.Tests
+{
+    public class ExecutorTest
+    {
+        [Fact]
+        public void TestConfigValid()
+        {
+            var exec_ = new Executor("config/valid.cfg");
+            Assert.True(exec_.IsLoadConfigValid); 
+        }
+
+        [Fact]
+        public void TestConfigInvalid()
+        {
+            var exec_ = new Executor("config/invalid.cfg");
+            Assert.False(exec_.IsLoadConfigValid); 
+        }
+    }
+}
