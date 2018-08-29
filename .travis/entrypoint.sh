4c19ab38624b24e75d67f37340be0b9359d5d22c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-08-29 19:00:43 -0300

    docker entrypoint fixes 7

--- .travis/entrypoint.sh
+++ .travis/entrypoint.sh
@@ -44,6 +44,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 # sudo apt install apt-transport-https
 echo "deb https://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt install -y mono-devel
+sudo apt install -y mono-complete
 
 
 # ------------------------------------------------------------
