9f0ec8ba864da8f939f9c90fba4e19e9867368d4
Dario Ramos <dario.fenix@gmail.com>
2018-10-03 17:57:49 -0300

    [CS-108] Roll back console tests

--- bitprim.console/Program.cs
+++ bitprim.console/Program.cs
@@ -8,7 +8,7 @@ namespace bitprim.console
 {
     public class Program
     {
-        //private static bool running_;
+        private static bool running_;
 
         static void Main(string[] args)
         {
@@ -35,22 +35,15 @@ namespace bitprim.console
                     {
                         throw new ApplicationException("Executor::InitAndRunAsync failed; error code: " + result);
                     }
-                    //executor.SubscribeToBlockChain(OnBlockArrived);
+                    executor.SubscribeToBlockChain(OnBlockArrived);
                     Log.Information("Synchronizing local copy of the blockchain...");
-                    // running_ = true;
-                    // while (running_)
-                    // {
-                    //     var lastHeight = await executor.Chain.FetchLastHeightAsync();
-                    //     Log.Information("Current height in local copy: " + lastHeight.Result);
-                    //     await Task.Delay(5000);
-                    // }
-                    await Task.Delay(10000);
-                    var addresses = new PaymentAddressList();
-                    addresses.Add(new PaymentAddress("bchtest:qq6g5362emyqppwx6kwpsl08xkgep7xwkyh9p68qsj"));
-                    addresses.Add(new PaymentAddress("bchtest:qqg2fwfzd4xeywf8h2zajqy77357gk0v7yvsvhd4xu"));
-                    var txs = executor.Chain.GetMempoolTransactions(addresses, true);
-                    Log.Information("Txs count: " + txs.Count);
-
+                    running_ = true;
+                    while (running_)
+                    {
+                        var lastHeight = await executor.Chain.FetchLastHeightAsync();
+                        Log.Information("Current height in local copy: " + lastHeight.Result);
+                        await Task.Delay(5000);
+                    }
                     Log.Information("Stopping node...");
                     executor.Stop();
                     Log.Information("Shutting down node...");
@@ -60,7 +53,6 @@ namespace bitprim.console
             {
                 Log.Error(ex,"Error detected");
             }
-
             Log.CloseAndFlush();
             Log.Information("Node shutdown OK!");
         }
@@ -68,7 +60,7 @@ namespace bitprim.console
         private static void OnSigInterrupt(object sender, ConsoleCancelEventArgs args)
         {
             args.Cancel = true;
-            //running_ = false;
+            running_ = false;
         }
 
         private static bool OnBlockArrived(ErrorCode errorCode, UInt64 u, BlockList incoming, BlockList outgoing)
