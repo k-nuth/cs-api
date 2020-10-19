a3ce6813f382f02069a62dfb266abcc86e4ccdd8
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-10-19 13:04:10 -0300

    use of c-api 0.5.2

--- console/Demo.cs
+++ console/Demo.cs
@@ -11,31 +11,32 @@ namespace console
     public class Program {
         static async Task Main(string[] args) {
             
-            // var config = Knuth.Config.Settings.GetFromFile("/home/fernando/dev/kth/cs-api/console/node.cfg");
-            var config = Knuth.Config.Settings.GetFromFile("/Users/fernando/dev/kth/cs-api/console/node_macos.cfg");
-            // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\console\\node_win.cfg");
-            // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\non_existing_file.cfg");
-            // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\invalid.cfg");
+//             // var config = Knuth.Config.Settings.GetFromFile("/home/fernando/dev/kth/cs-api/console/node.cfg");
+//             var config = Knuth.Config.Settings.GetFromFile("/Users/fernando/dev/kth/cs-api/console/node_macos.cfg");
+//             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\console\\node_win.cfg");
+//             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\non_existing_file.cfg");
+//             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\invalid.cfg");
 
-            if ( ! config.Ok) {
-                Console.WriteLine(config.ErrorMessage);
-                return;
-            }
+//             if ( ! config.Ok) {
+//                 Console.WriteLine(config.ErrorMessage);
+//                 return;
+//             }
 
-            Console.WriteLine("Starting demo");
+//             Console.WriteLine("Starting demo");
 
-#if KTH_CS_CURRENCY_BCH
-            Console.WriteLine("Currency: BCH");
-#else
-            Console.WriteLine("Currency: other");
-#endif
+// #if KTH_CS_CURRENCY_BCH
+//             Console.WriteLine("Currency: BCH");
+// #else
+//             Console.WriteLine("Currency: other");
+// #endif
 
-            // var config = Knuth.Config.Settings.GetDefault(NetworkType.Mainnet);
+            var config = Knuth.Config.Settings.GetDefault(NetworkType.Mainnet);
 
             Console.WriteLine("Launching node...");
 
-            using (var node = new Knuth.Node(config.Result, true)) {
+            // using (var node = new Knuth.Node(config.Result, true)) {
             // using (var node = new Knuth.Node(config, true)) {
+            using (var node = new Knuth.Node(config)) {
                 var res = await node.LaunchAsync();
                 Console.WriteLine(res);
 
