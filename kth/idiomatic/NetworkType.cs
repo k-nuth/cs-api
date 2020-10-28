537904470c211316d057de6c3c107343cb370977
Fernando pelliccioni <fpelliccioni@gmail.com>
2020-10-28 17:57:48 -0300

    added testnet4 and scalenet

--- kth/idiomatic/NetworkType.cs
+++ kth/idiomatic/NetworkType.cs
@@ -5,25 +5,14 @@
 namespace Knuth
 {
     /// <summary>
-    /// Represents the network type (Mainnet, Testnet, Regtest)
+    /// Represents the network type (Mainnet, Testnet, Regtest, ...)
     /// </summary>
     public enum NetworkType
     {
-        /// <summary>
-        /// Default value
-        /// </summary>
-        None = 0,
-        /// <summary>
-        /// Main network
-        /// </summary>
-        Mainnet = 1,
-        /// <summary>
-        /// Test network
-        /// </summary>
-        Testnet = 2,
-        /// <summary>
-        /// Regression test network
-        /// </summary>
-        Regtest = 3
+        Mainnet
+        , Testnet
+        , Regtest
+        , Testnet4
+        , Scalenet
     }
 }
