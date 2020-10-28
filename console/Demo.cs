0e36da9e874af4bb9ad2a7385fdea5cf676a6df0
Fernando pelliccioni <fpelliccioni@gmail.com>
2020-10-28 17:54:48 -0300

    readcash example

--- console/Demo.cs
+++ console/Demo.cs
@@ -1,65 +1,65 @@
-﻿// Copyright (c) 2016-2020 Knuth Project developers.
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+﻿// // Copyright (c) 2016-2020 Knuth Project developers.
+// // Distributed under the MIT software license, see the accompanying
+// // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-using System;
-using System.Threading.Tasks;
-using Knuth;
+// using System;
+// using System.Threading.Tasks;
+// using Knuth;
 
-namespace console
-{
-    public class Program {
-        static async Task Main(string[] args) {
+// namespace console
+// {
+//     public class Program {
+//         static async Task Main(string[] args) {
             
-//             // var config = Knuth.Config.Settings.GetFromFile("/home/fernando/dev/kth/cs-api/console/node.cfg");
-//             var config = Knuth.Config.Settings.GetFromFile("/Users/fernando/dev/kth/cs-api/console/node_macos.cfg");
-//             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\console\\node_win.cfg");
-//             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\non_existing_file.cfg");
-//             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\invalid.cfg");
+// //             // var config = Knuth.Config.Settings.GetFromFile("/home/fernando/dev/kth/cs-api/console/node.cfg");
+// //             var config = Knuth.Config.Settings.GetFromFile("/Users/fernando/dev/kth/cs-api/console/node_macos.cfg");
+// //             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\console\\node_win.cfg");
+// //             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\non_existing_file.cfg");
+// //             // var config = Knuth.Config.Settings.GetFromFile("C:\\development\\kth\\cs-api\\tests\\bch\\config\\invalid.cfg");
 
-//             if ( ! config.Ok) {
-//                 Console.WriteLine(config.ErrorMessage);
-//                 return;
+// //             if ( ! config.Ok) {
+// //                 Console.WriteLine(config.ErrorMessage);
+// //                 return;
+// //             }
+
+// //             Console.WriteLine("Starting demo");
+
+// // #if KTH_CS_CURRENCY_BCH
+// //             Console.WriteLine("Currency: BCH");
+// // #else
+// //             Console.WriteLine("Currency: other");
+// // #endif
+
+//             var config = Knuth.Config.Settings.GetDefault(NetworkType.Mainnet);
+
+//             Console.WriteLine("Launching node...");
+
+//             // using (var node = new Knuth.Node(config.Result, true)) {
+//             using (var node = new Knuth.Node(config, true)) {
+//             // using (var node = new Knuth.Node(config)) {
+//                 var res = await node.LaunchAsync();
+//                 Console.WriteLine(res);
+
+//                 // Console.WriteLine("Knuth node has been launched.");
+//                 // node.SubscribeBlockNotifications(OnBlockArrived);
+
+//                 var height = await node.Chain.GetLastHeightAsync();
+//                 Console.WriteLine($"Current height in local copy: {height.Result}");
+
+//                 // if (node.Chain.IsStale) {
+//                 //     Console.WriteLine("Knuth is doing IBD.");
+//                 // }
+
+//                 // await DiscoverThePowerOfKnuth(node);
 //             }
+//         }
 
-//             Console.WriteLine("Starting demo");
+//         private static bool OnBlockArrived(ErrorCode errorCode, UInt64 height, BlockList incoming, BlockList outgoing) {
+//             Console.WriteLine($"{incoming.Count} blocks arrived!");
+//             return true;
+//         }
+//         public static async Task DiscoverThePowerOfKnuth(Node node) {
 
-// #if KTH_CS_CURRENCY_BCH
-//             Console.WriteLine("Currency: BCH");
-// #else
-//             Console.WriteLine("Currency: other");
-// #endif
-
-            var config = Knuth.Config.Settings.GetDefault(NetworkType.Mainnet);
-
-            Console.WriteLine("Launching node...");
-
-            // using (var node = new Knuth.Node(config.Result, true)) {
-            // using (var node = new Knuth.Node(config, true)) {
-            using (var node = new Knuth.Node(config)) {
-                var res = await node.LaunchAsync();
-                Console.WriteLine(res);
-
-                // Console.WriteLine("Knuth node has been launched.");
-                // node.SubscribeBlockNotifications(OnBlockArrived);
-
-                var height = await node.Chain.GetLastHeightAsync();
-                Console.WriteLine($"Current height in local copy: {height.Result}");
-
-                // if (node.Chain.IsStale) {
-                //     Console.WriteLine("Knuth is doing IBD.");
-                // }
-
-                // await DiscoverThePowerOfKnuth(node);
-            }
-        }
-
-        private static bool OnBlockArrived(ErrorCode errorCode, UInt64 height, BlockList incoming, BlockList outgoing) {
-            Console.WriteLine($"{incoming.Count} blocks arrived!");
-            return true;
-        }
-        public static async Task DiscoverThePowerOfKnuth(Node node) {
-
-        }
-    }
-}
+//         }
+//     }
+// }
---
+++ console/readcash.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) 2016-2020 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Knuth;
+
+namespace HelloKnuth {
+    public class Program {
+        private static bool running_;
+
+        static async Task Main(string[] args) {
+            Console.CancelKeyPress += OnSigInterrupt;
+
+            var config = Knuth.Config.Settings.GetDefault(NetworkType.Mainnet);
+            config.Database.DbMaxSize /= 1024;
+            config.Network.Threads = 1;
+            config.Chain.Cores = 1;
+            // Console.WriteLine($"Config: {JsonSerializer.Serialize(config)}");
+
+            using (var node = new Knuth.Node(config, true)) {
+                Console.WriteLine("Lauching Knuth node...");
+                var res = await node.LaunchAsync();
+                if (res != ErrorCode.Success) {
+                    Console.WriteLine("Error launching the node...");
+                    return;
+                }
+                
+                running_ = true;
+                Console.WriteLine("Knuth node has been launched.");
+
+                var height = await node.Chain.GetLastHeightAsync();
+                Console.WriteLine($"Current height in local copy: {height.Result}");
+
+                if (await ComeBackAfterTheBCHHardFork(node)) {
+                    Console.WriteLine("Bitcoin Cash has been created!");
+                }
+                Console.WriteLine("Closing the node...");
+            }
+            Console.WriteLine("Good bye!");
+        }
+
+        private static async Task<bool> ComeBackAfterTheBCHHardFork(Node node) {
+            UInt64 hfHeight = 478559;
+            while (running_) {
+                var res = await node.Chain.GetLastHeightAsync();
+                if (res.Result >= hfHeight) return true;
+                await Task.Delay(10000);
+            }
+            return false;
+        }
+
+        private static void OnSigInterrupt(object sender, ConsoleCancelEventArgs args) {
+            Console.WriteLine("Stop signal detected.");
+            args.Cancel = true;
+            running_ = false;
+        }
+    }
+}
