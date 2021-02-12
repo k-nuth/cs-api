e331b21857d20e5abe43199efea8099e063fe8de
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-12 16:40:14 -0300

    Fixes string marshalling

--- kth/native/Platform.cs
+++ kth/native/Platform.cs
@@ -24,7 +24,6 @@ namespace Knuth.Native
         [DllImport(Constants.KTH_C_LIBRARY)]
         public static extern void kth_platform_print_string(IntPtr str);
 
-        // [DllImport(Constants.KTH_C_LIBRARY, CharSet=Constants.KTH_OS_CHARSET)]
         [DllImport(Constants.KTH_C_LIBRARY, CharSet=CharSet.Ansi)]
         public static extern IntPtr kth_platform_allocate_and_copy_string_at(IntPtr ptr, UInt64 offset, string str);
     }
