abbee7a6930fdfb87e1226de1a4dd1714d8f692b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-12 17:53:35 -0300

    removes old code

--- kth/idiomatic/Config/Settings.cs
+++ kth/idiomatic/Config/Settings.cs
@@ -15,10 +15,6 @@ namespace Knuth.Config
             res.Database = DatabaseSettings.GetDefault(network);
             res.Network = NetworkSettings.GetDefault(network);
             res.Node = NodeSettings.GetDefault(network);
-
-            //TODO(fernando): remove this in c-api version > 0.8.0. It is a workaround.
-            res.Network.Services = 1;
-            res.Network.HostPoolCapacity = 1000;
             return res;
         }
 
