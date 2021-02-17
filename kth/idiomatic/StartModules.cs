41e130527db40d8b2546e73d6c5429e388fa54fe
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-17 11:13:30 -0300

    Enum to select how to start the node

---
+++ kth/idiomatic/StartModules.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+namespace Knuth
+{
+    public enum StartModules
+    {
+        All
+        , JustChain
+        , JustP2P
+    }
+}
