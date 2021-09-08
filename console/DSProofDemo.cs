2e2d94a9df523a1a14f275338b72798e567fc9ca
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-09-08 13:08:43 -0300

    add DS Proof demo

---
+++ console/DSProofDemo.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+using System.Threading.Tasks;
+using Knuth;
+
+namespace console
+{
+    public class Program {
+        static async Task Main(string[] args) {
+            var config = Knuth.Config.Settings.GetDefault(NetworkType.Mainnet);
+            config.Node.DsProofsEnabled = true;
+            using (var node = new Knuth.Node(config)) {
+                await node.LaunchAsync();
+                Console.WriteLine("Knuth node has been launched.");
+
+                node.SubscribeDsProofNotifications((err, dsp) => {
+                    Console.WriteLine($"ATTENTION: a Double-Spend attempt has been received!");
+                    return true;
+                });
+
+                var height = await node.Chain.GetLastHeightAsync();
+                Console.WriteLine($"Current height in local copy: {height.Result}");
+
+                if (node.Chain.IsStale) {
+                    Console.WriteLine("Knuth is doing IBD.");
+                }
+
+                await DiscoverThePowerOfKnuth(node);
+            }
+        }
+
+
+
+
+        public static async Task DiscoverThePowerOfKnuth(Node node) {
+
+        }
+    }
+}
